204c25dc7bea9a2c4aa79a8771ad50c820e4df54
Somnath Prasad <somnathprasad559@gmail.com>
2021-03-31 09:53:44 +0530

    multiplayer cars working

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const io = require("socket.io")(http)
 const mongoose = require("mongoose");
 const { join } = require("path");
 const { count } = require("console");
+const { constants } = require("buffer");
 const parah = require(__dirname+"/RandomParah.ejs");
 const gen_Room_id = require(__dirname+"/RandomRoom_id.ejs");
 
@@ -18,7 +19,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb+srv://admin-somnath:hn8qM0QtxRoeZlkN@cluster0.vc3ep.mongodb.net/typingPracticeDB?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex:true
+  useCreateIndex:true   
 });
 
 
@@ -27,7 +28,8 @@ const roomSchema = new mongoose.Schema({
     room_id:String,
     members:[{
         member:String,
-        memberCount:Number
+        memberCount:Number,
+        isLeader:Boolean
     }]
 });
 
@@ -41,10 +43,15 @@ const Member = new mongoose.model("Member",memberSchema)
 
 
 var generatedRoom_id;
+var message="";
+var roomAndPlayers = [];
+var passCarNo=0;
+var passRoomId = "";
+var passName="";
 
 
 app.get("/",(req,res)=>{
-    res.render("home",{msg:""})
+    res.render("home",{msg:message})
 });
 
 app.post("/",(req,res)=>{
@@ -59,29 +66,33 @@ app.post("/",(req,res)=>{
 
 app.get("/createRoom",(req,res)=>{
     generatedRoom_id = gen_Room_id.getRoom_id();
-    res.render("createRoom",{room_id:"ROOM ID : "+generatedRoom_id,msg:""});
+    res.render("createRoom",{room_id:"ROOM ID : "+generatedRoom_id,msg:message});
+    message = "";
 });
 
 app.post("/createRoom",(req,res)=>{
 
     if(req.body.username===""){
         //if name box is empty
-        res.render("createRoom",{room_id:"ROOM ID : "+generatedRoom_id,msg:"Type Your Name"});
+        message = "Type Your Name"
+        res.redirect("/createRoom");
     }else{
         //if name box is not empty
         Room.findOne({room_id:generatedRoom_id},function(err,foundRoom){
             if(foundRoom !== null){
                 //room if already present with that id
-                res.render("createRoom",{room_id:"ROOM ID : "+generatedRoom_id,msg:"room already present please refresh page"})
+                message="room already present please refresh page"
+                // console.log(foundRoom)
+                res.redirect("/createRoom")
             }else{
                 //no room is already present in db creating one and redirecting to main game page
                 const room = new Room({
                     room_id:generatedRoom_id,
-                    members:[{member:req.body.username,memberCount:1}]
+                    members:[{member:req.body.username,memberCount:1,isLeader:true}]
                         });
                     room.save();
 
-                    res.redirect("/"+generatedRoom_id+"-"+req.body.username+"/carno/"+1);
+                    res.redirect("/"+generatedRoom_id+"-"+req.body.username+"/carno/"+"1"+"/isLeader/"+"true/");
             }
         });
     }
@@ -90,19 +101,21 @@ app.post("/createRoom",(req,res)=>{
 
 
 app.get("/joinRoom",(req,res)=>{
-    res.render("joinRoom",{msg:""});
+    res.render("joinRoom",{msg:message});
+    message="";
 });
 
 app.post("/joinRoom",(req,res)=>{
     if(req.body.username===""||req.body.id === ""){
-        res.render("joinRoom",{msg:"Please fill everything!"});
+        message="Please fill everything!"
+        res.redirect("/joinRoom");
     }else{
         // check if room is present or not
         Room.findOne({room_id:req.body.id},function(err,foundRoom){
             if(foundRoom !== null){
+                // console.log(foundRoom)
                 // room is present
                 // join player into index
-
                 var memberCount = 0 //to count the number of members already present in room
     
                 foundRoom.members.forEach(member => {
@@ -111,33 +124,135 @@ app.post("/joinRoom",(req,res)=>{
 
                 if(memberCount === 3){
                     // check if the room is full or not
-                    res.render("joinRoom",{msg:"Room full"});
+                    message="Room full"
+                    res.redirect("joinRoom");
                 }else{
                     // room not full 
                     // push new member into db and redirect to index
-                    foundRoom.members.push({member:req.body.username,memberCount:memberCount+1})
+                    foundRoom.members.push({member:req.body.username,memberCount:memberCount+1,isLeader:false})
                     foundRoom.save();
-                    res.redirect("/"+req.body.id+"-"+req.body.username+"/carno/"+memberCount);
+                    memberCount++;
+                    res.redirect("/"+req.body.id+"-"+req.body.username+"/carno/"+memberCount+"/isLeader/"+"false/");
                 }
                 
             }else{
                 //room not present
-                res.render("joinRoom",{msg:"Room not present"});
+                message="Room not present"
+                res.redirect("/joinRoom");
             }
         });
     }
 });
 
-app.get("/:room_id-:name/carno/:carno",(req,res)=>{
+app.get("/:room_id-:name/carno/:carno/isLeader/:isLeader/",(req,res)=>{
     //1. get room_id and name and pass it to js to make his car
-    const carNo = parseInt(req.params.carno)+1;
+    const carNo = parseInt(req.params.carno);
     passCarNo = carNo;
     const room_id = req.params.room_id;
     passRoomId = room_id;
     const name = req.params.name;
     passName = name;
-    res.render("index");
+    const isLeader = req.params.isLeader;
+
+    //storing every active room and its players details in an array "roomAndPlayers"
+
+    //structure of roomAndPlayers = [  {room_id , players[{array of objects}]  } ]
+
+    if(req.params.isLeader==="true"){
+        //this will create a new object inside main roomAndPlayers array with a new room_id
+        roomAndPlayers.push({room_id:req.params.room_id,players:[{name:req.params.name,carNo:carNo,isLeader:isLeader,room_id:req.params.room_id}]});
+        res.render("index",{startButton:"display:block;"});
+    }else{
+        roomAndPlayers.forEach((room,pos)=>{
+            if(room.room_id === passRoomId){
+                //this will push the details of new player inside already created players array
+                roomAndPlayers[pos].players.push({name:req.params.name,carNo:carNo,isLeader:isLeader,room_id:req.params.room_id})
+                res.render("index",{startButton:"display:none;"});
+            }
+            
+        })
+        
+    }
+    
 }); 
+
+//////////////////////////////////////When a player joins a game///////////////////////////////
+io.on("connection",(socket)=>{
+
+    
+    socket.join(passRoomId);
+    
+            allPlayers = [];
+            roomAndPlayers.forEach((room,pos)=>{
+                        if(room.room_id === passRoomId){
+                            allPlayers = roomAndPlayers[pos].players
+                            io.to(passRoomId).emit("new member",allPlayers);
+                        }
+                    });
+
+/////////////////////// When start button is clicked /////////////////////////////////////
+socket.on("start game",msg=>{
+    io.to(passRoomId).emit("parah",parah.getParah());
+});
+
+socket.on("car1_pos",pos=>{
+    io.to(passRoomId).emit("car1_moved",pos);
+});
+socket.on("car2_pos",pos=>{
+    io.to(passRoomId).emit("car2_moved",pos);
+});
+socket.on("car3_pos",pos=>{
+    io.to(passRoomId).emit("car3_moved",pos);
+});
+socket.on("car1_finished",msg=>{
+    io.to(passRoomId).emit("")
+})
+
+/////////////////////////////////When a player leaves the game//////////////////////////
+
+        //called when a player leaves the page
+    socket.on("leaving",(playerDetails)=>{
+        io.to(passRoomId).emit("player left",playerDetails);
+        console.log ("left: "+playerDetails.room_id+" "+playerDetails.name);
+
+        roomAndPlayers.forEach((room,pos1)=>{
+            if(room.room_id === playerDetails.room_id){
+
+                if(roomAndPlayers[pos1].players.length === 1){
+                    //if their is only one player left in the room 
+                    //this will delete the whole room
+                    roomAndPlayers.splice(pos1,1); //delete from server
+                    Room.findOneAndDelete({room_id:playerDetails.room_id},function(err,deletedRoom){
+                        console.log(deletedRoom);
+                    }) //delete from db.
+                }else{
+                    //this will delete the player who has left
+
+                    //delete player from db
+                    Room.findOne({room_id:playerDetails.room_id},function(err,foundRoom){
+                        if(foundRoom !== null){
+                            foundRoom.members.forEach((member,posDb)=>{
+                                if(member.member === playerDetails.name){
+                                    foundRoom.members.splice(posDb,1);
+                                    foundRoom.save();
+                                }
+                            });    
+                        }
+                    });
+
+                    //delete player from server
+                    roomAndPlayers[pos1].players.forEach((player,pos2)=>{
+                        if(player.name === playerDetails.name){
+                            roomAndPlayers[pos1].players.splice(pos2,1);
+                        }
+                        
+                    })
+                }   
+            }
+        });
+    });
+///////////////////////////////////////////////////////////////////////////////////////////////
+});
 
 http.listen(3000,()=>{
     console.log("server started on port 3000")
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 var socket = io();
-var raceTrack = document.getElementById("raceTrack");
 var startBtn = document.getElementById("startBtn");
 var parah = document.getElementById("parah");
 var spanLetters = [];
@@ -7,34 +6,49 @@ var cursorCount = 0;
 var typedLetter = "";
 var carNo = 0;
 var isCarNoSet = false;
+var playerName = "";
+var room_id="";
+var socketCalls = 0;
+var car = ""
 
-socket.on("first member",(member)=>{
-    carNo = member.carNo;
-    isCarNoSet = true;
-    createCar(member.carNo)
-    createName(member.name,member.carNo);
-});
+
+   
+
+
+//whenever a new member enter the room this is trigirred
 socket.on("new member",(members)=>{
-    if(!isCarNoSet){
-        carNo = members[members.length-1].carNo;
-        isCarNoSet = true;
-    }
-    var allCars = document.getElementsByClassName("car");
-    var allnames = document.getElementsByClassName("name");
-    for(var x=0;x<allCars.length;x++){
-        allCars[x].remove();
-        allnames[x].remove();
-    }
-    for(var x=0;x<members.length;x++){
-        createCar(members[x].carNo)
-        createName(members[x].name,members[x].carNo);
-    }
+    //raceTrack is the div containing car image and name element.
+    //evey time a new member enter the room , raceTrack div is emptied and filled with cars and names again.
+        var raceTrack = document.getElementById("raceTrack");
+
+        room_id = members[0].room_id;
+        
+        //this will set the car no of each player once and for all
+        if(!isCarNoSet){
+            carNo = members[members.length-1].carNo;
+            playerName = members[members.length-1].name;
+            isCarNoSet = true;
+        }
+
+        raceTrack.innerHTML = "";
+        
+        for(var x=0;x<members.length;x++){
+            createCar(members[x].carNo)
+            createName(members[x].name,members[x].carNo);
+        }
 });
 
-socket.on("delete",member=>{
-    var car2 = document.getElementById("car2");
-    console.log(car2);
-})
+//delete every player who leaves the game
+socket.on("player left",leftPlayer=>{
+    var leftCarNo = document.getElementById("car"+leftPlayer.carNo);
+    leftCarNo.remove();
+    var leftPlayerName = document.getElementById("name"+leftPlayer.carNo);
+    leftPlayerName.remove();
+    console.log(leftCarNo);
+});
+
+startBtn.style.cursor = "pointer";
+
 
 socket.on("parah",(parah)=>{
     createRandomParah(parah);
@@ -43,6 +57,7 @@ socket.on("parah",(parah)=>{
         typedLetter = spanLetters[cursorCount].innerHTML;
         startgame();
     }, 5000);
+    
 })
 
 function startgame(){
@@ -52,6 +67,8 @@ function startgame(){
             spanLetters[cursorCount].classList.remove("typing")
         }, 500);
     }, 1000);
+
+    
 
     document.addEventListener("keydown",(event)=>{
         if(event.key === typedLetter){  
@@ -67,17 +84,63 @@ function startgame(){
     }
     })
 }
-function moveCar(){
-    if(carNo == 1){
-        car = document.getElementById("car1");
-    }else if(carNo === 2){
-        car = document.getElementById("car2");
+function moveOtherCars(position){
+
+    if(carNo===1){
+        position.forEach((pos,index) => {
+            otherCar = document.getElementById("car"+(parseInt(index)+2));
+            otherCar.style.left = pos+'px';
+        });
+    }else if(carNo===2){
+        position.forEach((pos,index) => {
+            if(index === 0){
+                otherCar = document.getElementById("car1");
+                otherCar.style.left = pos+'px';
+            }else{
+                otherCar = document.getElementById("car3");
+                otherCar.style.left = pos+'px';
+            }
+            
+        });
     }else{
-        car = document.getElementById("car3");
+        position.forEach((pos,index) => {
+            otherCar = document.getElementById("car"+(parseInt(index)+1));
+            otherCar.style.left = pos+'px';
+        });
     }
-    car.style.left = parseInt(car.style.left)+9+'px';
-    socket.emit("car"+carNo+" position",parseInt(car.style.left));
 }
+
+//function for moving own car
+function moveCar(){
+        car = document.getElementById("car"+carNo);
+        car.style.left = parseInt(car.style.left)+9+'px';
+        socket.emit("car"+carNo+"_pos",car.style.left);
+        if(car.style.left === 1153){
+            socket.emit("car"+carNo+"_finished","finished")
+        }
+}
+
+//sockets for moving other cars
+socket.on("car1_moved",pos=>{
+    if(carNo !== 1){
+        var car1 = document.getElementById("car1");
+        car1.style.left = pos;
+    }
+});
+socket.on("car2_moved",pos=>{
+    if(carNo !== 2){
+        var car2 = document.getElementById("car2");
+        car2.style.left = pos;
+    }
+});
+socket.on("car3_moved",pos=>{
+    if(carNo !== 3){
+        var car3 = document.getElementById("car3");
+        car3.style.left = pos;
+    }
+});
+
+
 
 function createCar(carNo){
     var newCar = document.createElement("img");
@@ -111,4 +174,8 @@ startBtn.addEventListener("click",()=>{
     if(allCars.length>1){
         socket.emit("start game","new parah")
     }
-})
+    
+});
+window.addEventListener("beforeunload",function(e){
+  socket.emit("leaving",{name:playerName,room_id:room_id,carNo:carNo}) 
+});
